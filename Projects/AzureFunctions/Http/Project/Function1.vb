7f522f98dc8174628ee04cc73d8fbdfcd50bc133
Jeff Bowman <10966749+InteXX@users.noreply.github.com>
2020-06-01 21:10:03 -0800

    Roll up NuGet package

--- Projects/AzureFunctions/Http/Project/Function1.vb
+++ Projects/AzureFunctions/Http/Project/Function1.vb
@@ -5,25 +5,27 @@ Imports Microsoft.AspNetCore.Http
 Imports Microsoft.Extensions.Logging
 Imports Microsoft.Extensions.Primitives
 
-Public Module Function1
-  <FunctionName("Function1")>
-  Public Function Run(<HttpTrigger(AuthorizationLevel.Function, {"POST", "GET"}, Route:=Nothing)> Request As HttpRequest, Logger As ILogger) As IActionResult
-    Dim oValue As StringValues
-    Dim sName As String
+Namespace Intexx.Templates.Projects.AzureFunctions.HttpTrigger
+  Public Module Function1
+    <FunctionName("Function1")>
+    Public Function Run(<HttpTrigger(AuthorizationLevel.Function, {"POST", "GET"}, Route:=Nothing)> Request As HttpRequest, Logger As ILogger) As IActionResult
+      Dim oValue As StringValues
+      Dim sParam As String
 
-    Logger.LogInformation("VB HTTP trigger function processed a request.")
+      Logger.LogInformation("VB HTTP trigger function processed a request.")
 
-    sName = "name"
+      sParam = "name"
 
-    Select Case Request.Method.ToUpper
-      Case "POST" : oValue = Request.Form(sName)
-      Case "GET" : oValue = Request.Query(sName)
-    End Select
+      Select Case Request.Method.ToUpper
+        Case "POST" : oValue = Request.Form(sParam)
+        Case "GET" : oValue = Request.Query(sParam)
+      End Select
 
-    If oValue.Count = 0 OrElse oValue = String.Empty Then
-      Run = New BadRequestObjectResult("You must pass a name on the query string or in the request body.")
-    Else
-      Run = New OkObjectResult($"Hello, {oValue}!")
-    End If
-  End Function
-End Module
+      If oValue.Count = 0 OrElse oValue = String.Empty Then
+        Run = New BadRequestObjectResult("You must pass a name on the query string or in the request body.")
+      Else
+        Run = New OkObjectResult($"Hello, {oValue}!")
+      End If
+    End Function
+  End Module
+End Namespace
